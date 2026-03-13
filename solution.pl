9088f8b205605db06257e4922922fc2531a09105
Alexandru Teșulă <alexandru.tesula@student.unitbv.ro>
2026-03-13 16:36:17 +0200

    adauga rezolvarea recursiv

--- solution.pl
+++ solution.pl
@@ -10,3 +10,18 @@ mutare(stare(T1, L, T1, V), stare(T2, L, T2, V)) :-
 % Taran + varza.
 mutare(stare(T1, L, C, T1), stare(T2, L, C, T2)) :- 
     opus(T1, T2).
+
+% Cazul de succes: Toate elementele au ajuns pe malul drept ('dr').
+rezolva(stare(dr, dr, dr, dr), _, [stare(dr, dr, dr, dr)]).
+
+% Recursivitatea.
+rezolva(StareCurenta, Vizitate, [StareCurenta | Drum]) :-
+    mutare(StareCurenta, StareUrmatoare),
+    valid(StareUrmatoare),
+    \+ member(StareUrmatoare, Vizitate), % Verificam sa nu mergem inapoi
+    rezolva(StareUrmatoare, [StareCurenta | Vizitate], Drum).
+
+% Startul programului, toti incep de la stanga ('stg').
+solutie(Solutie) :-
+    StareInitiala = stare(stg, stg, stg, stg),
+    rezolva(StareInitiala, [], Solutie).
